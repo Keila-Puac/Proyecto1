3a561ac6aecb3c9539ad05856c404595485e2098
Keila-Puac <Keilapuac3@gmail.com>
2025-09-14 12:06:48 -0600

    Registro de usuarios Estudiantes/Docentes

--- PrimeraParte.py
+++ PrimeraParte.py
@@ -5,19 +5,34 @@
 #Registrar Estudiantes y Profesores 
 
 diccionario_usuarios =  {"Nombre":"carnet"}
+lista_estudiantes = {}
+lista_docentes = {}
 
 
 class Registrar:
     def __init__(self, nombre, carnet):
-        self.nombre = nombre
-        self.carnet = carnet
+        self.__nombre = nombre
+        self.__carnet = carnet
 
+    def registroEstudiante(self):
         try:
-            nombre_usuario = input("Ingrese su nombre")
-            carnet_usuario = input("Ingrese su carnet")
+            self.__nombre = input("Ingrese su nombre y apellido")
+            self.__carnet = input("Ingrese su carnet")
+            lista_estudiantes[self.__carnet] = self.__nombre
+            print(lista_estudiantes)
         except ValueError:
             print("Los valores ingresados no son correctos.")
 
-        
+    def registroDocente(self):
+        try:
+            self.__nombre = input("Ingrese su nombre y apellido")
+            self.__carnet = input("Ingrese su carnet")
 
-    
+            
+        except ValueError:
+            print("Los valores ingresados son incorrectos")
+
+
+usuario1 = Registrar("Paula", "12548")
+usuario1.registroEstudiante()
+
