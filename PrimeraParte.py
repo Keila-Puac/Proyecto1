b4cc726054c18c7ec4f88f2ee1d896af8dd6a34b
Keila-Puac <Keilapuac3@gmail.com>
2025-09-15 12:02:02 -0600

    Clase curso/ crear curso

--- PrimeraParte.py
+++ PrimeraParte.py
@@ -4,10 +4,10 @@
 
 #Registrar Estudiantes y Profesores 
 
-diccionario_usuarios =  {"Nombre":"carnet"}
 lista_estudiantes = {}
 lista_docentes = {}
-
+lista_cursos = {}
+#lista_cursos = {"Código Curso":{"Nombre Curso":"Instructor"}}
 
 class Registrar:
     def __init__(self, nombre, carnet):
@@ -16,23 +16,55 @@ class Registrar:
 
     def registroEstudiante(self):
         try:
-            self.__nombre = input("Ingrese su nombre y apellido")
-            self.__carnet = input("Ingrese su carnet")
+            self.__nombre = input("Ingrese su nombre y apellido: ")
+            self.__carnet = input("Ingrese su carnet: ")
             lista_estudiantes[self.__carnet] = self.__nombre
-            print(lista_estudiantes)
+            print("Usuario Registrado con éxito")
         except ValueError:
             print("Los valores ingresados no son correctos.")
 
     def registroDocente(self):
         try:
-            self.__nombre = input("Ingrese su nombre y apellido")
-            self.__carnet = input("Ingrese su carnet")
+            self.__nombre = input("Ingrese su nombre y apellido: ")
+            self.__carnet = input("Ingrese su carnet: ")
+            lista_docentes[self.__carnet] = self.__nombre
+            print("Usuario Registrado con éxito")
 
             
         except ValueError:
             print("Los valores ingresados son incorrectos")
 
 
-usuario1 = Registrar("Paula", "12548")
-usuario1.registroEstudiante()
+#Inscribir a estudiantes en cursos
 
+class Cursos:
+    def __init__(self, nombre_curso, código_curso, instructor):
+        self.__nombre_curso = nombre_curso
+        self.__código_curso = código_curso
+        self.__instructor = instructor
+
+    def Crear(self):
+        try:
+            self.__nombre_curso = input("Ingrese el nombre del curso: ")
+            self.__código_curso = input("Ingrese el código del curso: ")
+            self.__instructor = input("Ingrese el nombre del instructor/docente: ")
+            
+            if self.__instructor in lista_docentes:
+                lista_cursos[self.__código_curso] = {
+                    "Nombre curso": self.__nombre_curso,
+                    "Instructor": lista_docentes[self.__instructor]
+                }
+                print("Curso creado exitosamente")
+                print(lista_cursos)
+            else:
+                print("El docente no esta registrado")
+
+
+        except ValueError:
+            print("Los datos ingresados no son validos")
+
+
+        
+
+Curso1 = Cursos("", "", "")
+Curso1.Crear()
