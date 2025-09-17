1a643501bbb8e42f91a2e3ee849d0a0653e351ee
Morelia-Gonzalez <moreliagonzalezixcot@gmail.com>
2025-09-17 17:56:12 -0600

    Update calificaciones.py

--- calificaciones.py
+++ calificaciones.py
@@ -26,29 +26,36 @@ class Estudiante:
 # clase Sitema Academico para  consultar el promedio del estudiante y su rendimiento
 
 class SistemaAcademico:
+    #Clase que gestiona un sistema académico básico.
+    #Permite registrar estudiantes, listarlos, consultar calificaciones  por curso y generar reportes de promedios.
     def __init__(self):
           self.estudiantes = []
 
 #Funcion para agregar a un estudiante
-    def agregar_estudiante(self, estudiante):
+    def agregar_estudiante(self, estudiante): 
+            #Agrega un estudiante al sistema académico
+            # Args: estudiante. objeto de la clase estudiante que sera agregado  
             self.estudiantes.append(estudiante)
 
     #Funcion que devuelve una lista de estudiantes 
-    def listar_estudiantes(self):
+    def listar_estudiantes(self): 
+            #Devuelve una lista con los nombres de los estudiantes registrados 
             return [est.nombre for est in self.estudiantes]
     #Funcion para consultar al estudiante y su respectivo promedio
 
     def consultar_curso(self, curso):
         """Devuelve las evaluaciones y calificaciones de todos los estudiantes en un curso"""
+        #Args: Curso nombre del curso a consultar
         resultado = {}
         for estudiante in self.estudiantes:
             if curso in estudiante.calificaciones:
                 resultado[estudiante.nombre] = estudiante.calificaciones[curso]
-        return resultado
+        return resultado # diccionario con la estructura.
 
    # funcion para reporte de promedios
     def reporte_promedios(self, promedio_bajo=70):
         """Genera un reporte simple con promedios y alerta de promedio bajo"""
+        #Args. list[dict]: Lista de diccionarios con la informacion por estudiante 
         reporte = []
         for estudiante in self.estudiantes:
             promedio = estudiante.promedio_general()
